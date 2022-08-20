398a42629a64fbe099966814da83edc68d94ef5e
thenns <samm77@gmail.com>
2022-08-20 15:02:51 -0600

    fixed the formatting

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,7 +3,8 @@ for (let i = 1; i <= 50; i++){
     
     if (i % 5 === 0 && i % 3 === 0){
         console.log("fizz"+"buzz");
-    }else if (i % 3 === 0){
+    }
+    else if (i % 3 === 0){
         console.log("fizz");
     }
     else if (i % 5 === 0){
